7a7395459104384f117d980d4b833fbc55fbc29c
Smithienious <nhoang3b@yahoo.com.vn>
2020-11-08 23:51:35 +0700

    DSA: A1P2: Updated main.cpp

--- CO2003/a1p2/main.cpp
+++ CO2003/a1p2/main.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int main()
 {
+    /*
+     * SQ <N>
+     * INS <CODE> <LP> [<CASE>] <TIME_A> <TIME_B>
+     * DEL <CODE> [<TIME_A> [<TIME_B>]]
+     * CS <CODE> <TIME_A> [<CASE>]
+     * CE <CODE> <TIME_A> [<CASE>]
+     * GS <CODE> <TIME_A> [<CASE>]
+     * GE <CODE> <TIME_A> [<CASE>]
+     */
     BusSystem *bs = new BusSystem();
-    cout << bs->query("INS 50 50D1-23342 1234 5678") << endl;
-    cout << bs->query("CS 50 2134") << endl;
+    cout << bs->query("SQ 050 INS 50 50D1-23342 1234 5678") << endl;
+    //cout << bs->query("CS 50 2134") << endl;
 
     return 0;
 }

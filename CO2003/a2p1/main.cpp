e162496b407ad326d1b8629a058ebf97da6d8b4d
Smithienious <nhoang3b@yahoo.com.vn>
2020-11-22 02:18:28 +0700

    Added BKeL test cases

--- CO2003/a2p1/main.cpp
+++ CO2003/a2p1/main.cpp
@@ -3,18 +3,39 @@
  * @author NGUYEN Hoang (hoang.nguyen.bigunit@hcmut.edu.vn)
  * @brief Main runner for Assignment 2 Phase 1
  *
- * Learners are expected to be able to use BST, specifically AVL and Splay Tree
- *
- * @version 0.1
- * @date 2020-11-21
+ * @version 0.2
+ * @date 2020-11-22
  *
  * @copyright Copyright (c) 2020
- *
  ************/
 
 #include "BKUTree.hpp"
 #include <iostream>
 
-int main(int argc, char** argv) {
+void printKey(int key, int value)
+{
+    cout << key << endl;
+}
+
+void BKeL1() // 3 1 2 7 5 4 9
+{
+    BKUTree<int, int> *tree = new BKUTree<int, int>();
+    int keys[] = {1, 3, 5, 7, 9, 2, 4};
+    for (int i = 0; i < 7; i++)
+        tree->add(keys[i], keys[i]);
+    tree->traverseNLROnAVL(&printKey);
+}
+
+void BKeL2() // 4 2 1 3 9 5 7
+{
+    BKUTree<int, int> *tree = new BKUTree<int, int>();
+    int keys[] = {1, 3, 5, 7, 9, 2, 4};
+    for (int i = 0; i < 7; i++)
+        tree->add(keys[i], keys[i]);
+    tree->traverseNLROnSplay(&printKey);
+}
+
+int main(int argc, char **argv)
+{
     std::cout << "Hello, world!\n";
 }

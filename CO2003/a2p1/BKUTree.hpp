e379952994c2fc1f3aede7e8a78d5ab2297e80f0
Smithienious <nhoang3b@yahoo.com.vn>
2020-11-22 02:19:01 +0700

    Expanded methods

--- CO2003/a2p1/BKUTree.hpp
+++ CO2003/a2p1/BKUTree.hpp
@@ -4,19 +4,26 @@
  * @brief Main implementation for Assignment 2 Phase 1
  *
  * Learners are expected to be able to use BST, specifically AVL and Splay Tree
+ * AVL tree stores the BST structure, Splay tree stores recently accessed elements
  *
- * @version 0.1
- * @date 2020-11-21
+ * @version 0.1.1
+ * @date 2020-11-22
  *
  * @copyright Copyright (c) 2020
- *
  ************/
 
 #include <iostream>
 #include <queue>
 #include <vector>
+
 using namespace std;
 
+/************
+ * @brief Main structure
+ *
+ * @tparam K
+ * @tparam V
+ ************/
 template <class K, class V>
 class BKUTree
 {
@@ -24,6 +31,9 @@ public:
     class AVLTree;
     class SplayTree;
 
+    /************
+     * @brief Memory allocator
+     ************/
     class Entry
     {
     public:
@@ -31,7 +41,7 @@ public:
         V value;
 
         Entry(K key, V value) : key(key), value(value) {}
-    };
+    }; // class Entry
 
 private:
     AVLTree *avl;
@@ -43,15 +53,18 @@ public:
     BKUTree(int maxNumOfKeys = 5) {}
     ~BKUTree() { this->clear(); }
 
-    void add(K key, V value);
-    void remove(K key);
-    V search(K key, vector<K> &traversedList);
+    void add(K, V);
+    void remove(K);
+    V search(K, vector<K> &);
 
-    void traverseNLROnAVL(void (*func)(K key, V value));
-    void traverseNLROnSplay(void (*func)(K key, V value));
+    void traverseNLROnAVL(void (*)(K, V));
+    void traverseNLROnSplay(void (*)(K, V));
 
     void clear();
 
+    /************
+     * @brief Store recently accessed data with order
+     ************/
     class SplayTree
     {
     public:
@@ -62,31 +75,34 @@ public:
             Node *right;
             typename AVLTree::Node *corr;
 
-            Node(Entry *entry = NULL, Node *left = NULL, Node *right = NULL)
+            Node(Entry *entry = nullptr, Node *left = nullptr, Node *right = nullptr)
             {
                 this->entry = entry;
                 this->left = left;
                 this->right = right;
-                this->corr = NULL;
+                this->corr = nullptr;
             }
         };
 
     public:
         Node *root;
 
-        SplayTree() : root(NULL){};
+        SplayTree() : root(nullptr){};
         ~SplayTree() { this->clear(); };
 
-        void add(K key, V value);
-        void add(Entry *entry);
-        void remove(K key);
-        V search(K key);
+        void add(K, V);
+        void add(Entry *);
+        void remove(K);
+        V search(K);
 
-        void traverseNLR(void (*func)(K key, V value));
+        void traverseNLR(void (*)(K, V));
 
         void clear();
-    };
+    }; // class SplayTree
 
+    /************
+     * @brief Store the BST structure
+     ************/
     class AVLTree
     {
     public:
@@ -98,29 +114,280 @@ public:
             int balance;
             typename SplayTree::Node *corr;
 
-            Node(Entry *entry = NULL, Node *left = NULL, Node *right = NULL)
+            Node(Entry *entry = nullptr, Node *left = nullptr, Node *right = nullptr)
             {
                 this->entry = entry;
                 this->left = left;
                 this->right = right;
                 this->balance = 0;
-                this->corr = NULL;
+                this->corr = nullptr;
             }
         };
 
     public:
         Node *root;
 
-        AVLTree() : root(NULL){};
+        AVLTree() : root(nullptr){};
         ~AVLTree() { this->clear(); };
 
-        void add(K key, V value);
-        void add(Entry *entry);
-        void remove(K key);
-        V search(K key);
+        void add(K, V);
+        void add(Entry *);
+        void remove(K);
+        V search(K);
 
-        void traverseNLR(void (*func)(K key, V value));
+        void traverseNLR(void (*)(K, V));
 
         void clear();
-    };
-};
+    }; // class AVLTree
+};     // class BKUTree
+
+/************
+ * @brief Add an element with key and value to BKUTree
+ *        Throw "Duplicate key" if key exists
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @param value
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::add(K key, V value)
+{
+    // TODO
+}
+
+/************
+ * @brief Remove an element with key from BKUTree
+ *        Throw "Not found" if key does not exist
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::remove(K key)
+{
+    // TODO
+}
+
+/************
+ * @brief Find an element with key in BKUTree
+ *        Throw "Not found" if key does not exist
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @param traversedList store traversed nodes with order
+ * @return V
+ ************/
+template <class K, class V>
+V BKUTree<K, V>::search(K key, vector<K> &traversedList)
+{
+    // TODO
+}
+
+/************
+ * @brief Pre-order traversal on the AVL tree
+ *
+ * @tparam K
+ * @tparam V
+ * @param func
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::traverseNLROnAVL(void (*func)(K key, V value))
+{
+    // TODO
+}
+
+/************
+ * @brief Pre-order traversal on the Splay tree
+ *
+ * @tparam K
+ * @tparam V
+ * @param func
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::traverseNLROnSplay(void (*func)(K key, V value))
+{
+    // TODO
+}
+
+/************
+ * @brief Free everything
+ *
+ * @tparam K
+ * @tparam V
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::clear()
+{
+    // TODO
+}
+
+/************
+ * @brief Add an element with key and value to BKUTree
+ *        Throw "Duplicate key" if key exists
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @param value
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::SplayTree::add(K key, V value)
+{
+    // TODO
+}
+
+/************
+ * @brief Add an entry to BKUTree
+ *        Throw "Duplicate key" if entry exists
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @param value
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::SplayTree::add(Entry *entry)
+{
+    // TODO
+}
+
+/************
+ * @brief Remove an element with key from BKUTree
+ *        Throw "Not found" if key does not exist
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::SplayTree::remove(K key)
+{
+    // TODO
+}
+
+/************
+ * @brief Find an element with key in BKUTree
+ *        Throw "Not found" if key does not exist
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @return V
+ ************/
+template <class K, class V>
+V BKUTree<K, V>::SplayTree::search(K key)
+{
+    // TODO
+}
+
+/************
+ * @brief Pre-order traversal
+ *
+ * @tparam K
+ * @tparam V
+ * @param func
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::SplayTree::traverseNLR(void (*func)(K key, V value))
+{
+    // TODO
+}
+
+/************
+ * @brief Free everything
+ *
+ * @tparam K
+ * @tparam V
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::SplayTree::clear()
+{
+    // TODO
+}
+
+/************
+ * @brief Add an element with key and value to BKUTree
+ *        Throw "Duplicate key" if key exists
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @param value
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::AVLTree::add(K key, V value)
+{
+    // TODO
+}
+
+/************
+ * @brief Add an entry to BKUTree
+ *        Throw "Duplicate key" if entry exists
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @param value
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::AVLTree::add(Entry *entry)
+{
+    // TODO
+}
+
+/************
+ * @brief Remove an element with key from BKUTree
+ *        Throw "Not found" if key does not exist
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::AVLTree::remove(K key)
+{
+    // TODO
+}
+
+/************
+ * @brief Find an element with key in BKUTree
+ *        Throw "Not found" if key does not exist
+ *
+ * @tparam K
+ * @tparam V
+ * @param key
+ * @return V
+ ************/
+template <class K, class V>
+V BKUTree<K, V>::AVLTree::search(K key)
+{
+    // TODO
+}
+
+/************
+ * @brief Pre-order traversal
+ *
+ * @tparam K
+ * @tparam V
+ * @param func
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::AVLTree::traverseNLR(void (*func)(K key, V value))
+{
+    // TODO
+}
+
+/************
+ * @brief Free everything
+ *
+ * @tparam K
+ * @tparam V
+ ************/
+template <class K, class V>
+void BKUTree<K, V>::AVLTree::clear()
+{
+    // TODO
+}

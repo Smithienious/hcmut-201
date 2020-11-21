5b69c2a995773ed5fc4c80d8211bc9cb4af74780
Smithienious <nhoang3b@yahoo.com.vn>
2020-11-21 23:58:19 +0700

    Initialized A2P1

---
+++ CO2003/a2p1/main.cpp
@@ -0,0 +1,20 @@
+/************
+ * @file main.cpp
+ * @author NGUYEN Hoang (hoang.nguyen.bigunit@hcmut.edu.vn)
+ * @brief Main runner for Assignment 2 Phase 1
+ *
+ * Learners are expected to be able to use BST, specifically AVL and Splay Tree
+ *
+ * @version 0.1
+ * @date 2020-11-21
+ *
+ * @copyright Copyright (c) 2020
+ *
+ ************/
+
+#include "BKUTree.hpp"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, world!\n";
+}

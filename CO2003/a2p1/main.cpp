bcd65263bb86e8d9e379533483820de7d033db3b
Smithienious <nhoang3b@yahoo.com.vn>
2020-12-02 13:56:39 +0700

    Updated test cases

--- CO2003/a2p1/main.cpp
+++ CO2003/a2p1/main.cpp
@@ -11,32 +11,80 @@
 
 #include "BKUTree.hpp"
 #include <iostream>
+#include <iomanip>
 
 void printKey(int key, int value)
 {
-    std::cout << key << endl;
+    cout << key << endl;
 }
 
-void BKeL1() // 3 1 2 7 5 4 9
+void printValue(int key, int value)
 {
+    cout << value << endl;
+}
+
+void BKeL1()
+{
+    /**
+     *  3 1 2 7 5 4 9
+     **/
+
     BKUTree<int, int> *tree = new BKUTree<int, int>();
     int keys[] = {1, 3, 5, 7, 9, 2, 4};
     for (int i = 0; i < 7; i++)
         tree->add(keys[i], keys[i]);
     tree->traverseNLROnAVL(&printKey);
+
+    // * Custom
+    cout << "- - - - - - " << endl;
+    tree->printAVL();
+    tree->printSplay();
 }
 
-void BKeL2() // 4 2 1 3 9 5 7
+void BKeL2()
 {
+    /************
+     * 4 2 1 3 9 5 7
+     ************/
+
     BKUTree<int, int> *tree = new BKUTree<int, int>();
     int keys[] = {1, 3, 5, 7, 9, 2, 4};
     for (int i = 0; i < 7; i++)
         tree->add(keys[i], keys[i]);
     tree->traverseNLROnSplay(&printKey);
+
+    // * Custom
+    cout << "- - - - - - " << endl;
+    tree->printAVL();
+    tree->printSplay();
+}
+
+void LocalTest(int N = 0)
+{
+    if (N == 0)
+    {
+        std::cout << "Input number of nodes: ";
+        std::cin >> N;
+    }
+    BKUTree<int, int> *tree = new BKUTree<int, int>();
+
+    for (int i = 0; i < N; i++)
+    {
+        int a = rand() % 100;
+        tree->add(a, rand() % 1000);
+        std::cout << setw(5) << a;
+    }
+    tree->traverseNLROnSplay(&printKey);
+
+    // * Custom
+    cout << "- - - - - - " << endl;
+    tree->printAVL();
+    tree->printSplay();
 }
 
 int main(int argc, char **argv)
 {
     BKeL1();
+    cout << "------------" << endl;
     BKeL2();
 }

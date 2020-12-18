7cc9a225853a4a93befc46f6622a5f5a0b6954e7
Smithienious <nhoang3b@yahoo.com.vn>
2020-12-19 00:52:19 +0700

    Updated main.cpp

--- CO2003/a2p1/main.cpp
+++ CO2003/a2p1/main.cpp
@@ -3,8 +3,8 @@
  * @author NGUYEN Hoang (hoang.nguyen.bigunit@hcmut.edu.vn)
  * @brief Main runner for Assignment 2 Phase 1
  *
- * @version 0.3.0
- * @date 2020-12-16
+ * @version 0.3.1
+ * @date 2020-12-19
  *
  * @copyright Copyright (c) 2020
  ************/
@@ -51,29 +51,6 @@ void BKeL2()
   int keys[] = {1, 3, 5, 7, 9, 2, 4};
   for (int i = 0; i < 7; i++)
     tree->add(keys[i], keys[i]);
-  tree->traverseNLROnSplay(&printKey);
-
-  // * Custom
-  cout << "- - - - - - " << endl;
-  tree->printAVL();
-  tree->printSplay();
-}
-
-void LocalTest(int N = 0)
-{
-  if (N == 0)
-  {
-    std::cout << "Input number of nodes: ";
-    std::cin >> N;
-  }
-  BKUTree<int, int> *tree = new BKUTree<int, int>();
-
-  for (int i = 0; i < N; i++)
-  {
-    int a = rand() % 100;
-    tree->add(a, rand() % 1000);
-    std::cout << setw(5) << a;
-  }
   tree->traverseNLROnSplay(&printKey);
 
   // * Custom
